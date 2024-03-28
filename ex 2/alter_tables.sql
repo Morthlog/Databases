3643aec4fad06943878840cbdc25158cc4ca7a13
Morthlog <iosifpetroulakis@hotmail.com>
2024-03-28 15:20:03 +0200

    refactor: FR updated to FK

--- ex 2/alter_tables.sql
+++ ex 2/alter_tables.sql
@@ -27,32 +27,32 @@ ALTER TABLE keyword
 PRIMARY KEY (id);
 
 ALTER TABLE belongsTocollection
-    ADD CONSTRAINT FR_belong_movie_id FOREIGN
+    ADD CONSTRAINT FK_belong_movie_id FOREIGN
 KEY (movie_id)
     REFERENCES movie(id);
 
 ALTER TABLE belongsTocollection
-    ADD CONSTRAINT FR_belong_collection_id FOREIGN
+    ADD CONSTRAINT FK_belong_collection_id FOREIGN
 KEY (collection_id)
     REFERENCES collection(id);
 
 ALTER TABLE hasGenre
-    ADD CONSTRAINT FR_hasGenre_movie_id FOREIGN
+    ADD CONSTRAINT FK_hasGenre_movie_id FOREIGN
 KEY (movie_id)
     REFERENCES movie(id);
 
 ALTER TABLE hasGenre
-    ADD CONSTRAINT FR_hasGenre_genre_id FOREIGN
+    ADD CONSTRAINT FK_hasGenre_genre_id FOREIGN
 KEY (genre_id)
     REFERENCES genre(id);
 
 ALTER TABLE hasProductioncompany
-    ADD CONSTRAINT FR_hasPC_movie_id FOREIGN
+    ADD CONSTRAINT FK_hasPC_movie_id FOREIGN
 KEY (movie_id)
     REFERENCES movie(id);
 
 ALTER TABLE hasProductioncompany
-    ADD CONSTRAINT FR_hasPC_pc_id FOREIGN
+    ADD CONSTRAINT FK_hasPC_pc_id FOREIGN
 KEY (pc_id)
     REFERENCES productioncompany(id);
 
