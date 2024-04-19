fb0b5a1a9ea5dbfe76a9b697d8cc5bc6edb61591
BabisDros <150071850+BabisDros@users.noreply.github.com>
2024-04-19 15:54:33 +0300

    fix: change to name-surname format

--- ex 4/f.inconsistencies.sql
+++ ex 4/f.inconsistencies.sql
@@ -15,7 +15,7 @@ HAVING COUNT(name) > 1 OR COUNT(gender) > 1
 );
 
 UPDATE movie_crew
-SET name='Cheung Ka-Fai'  where person_id in (
+SET name='Ka-Fai Cheung'  where person_id in (
 SELECT person_id
 FROM person
 GROUP BY person_id
