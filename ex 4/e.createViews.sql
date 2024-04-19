3a1eae353f5e1726800da35c399f8f810aaa737d
BabisDros <150071850+BabisDros@users.noreply.github.com>
2024-04-19 16:39:34 +0300

    feat: add go statement to create views in succession

--- ex 4/e.createViews.sql
+++ ex 4/e.createViews.sql
@@ -2,9 +2,13 @@ CREATE VIEW Actor AS
 SELECT DISTINCT person_id, gender, name
 FROM movie_cast;
 
+Go -- used to run statements in succession
+
 CREATE VIEW CrewMember AS
 SELECT DISTINCT person_id, gender, name
 FROM movie_crew;
+
+Go -- used to run statements in succession
 
 CREATE VIEW Person AS
 (
