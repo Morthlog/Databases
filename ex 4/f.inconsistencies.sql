88aa695a07b98c7087096ae04b34cc22efc4192c
BabisDros <150071850+BabisDros@users.noreply.github.com>
2024-04-20 16:09:15 +0300

    fix: generic queries

--- ex 4/f.inconsistencies.sql
+++ ex 4/f.inconsistencies.sql
@@ -1,23 +1,82 @@
+/*=========Αντικατάσταση των διπλότυπων με το μέγιστο genre (υποθέτουμε οτι έχει δυο, το σωστό και το άγνωστο)===========*/
+UPDATE movie_crew
+SET gender = (
+    SELECT MAX(gender)
+    FROM (
+        SELECT person_id, gender
+        FROM movie_crew 
+        UNION
+        SELECT person_id, gender
+        FROM movie_cast
+    ) AS combined_data
+    WHERE combined_data.person_id = movie_crew.person_id
+)
+WHERE person_id IN (
+    SELECT person_id
+    FROM Person
+    GROUP BY person_id
+    HAVING COUNT(DISTINCT gender) > 1
+);
+
+
 UPDATE movie_cast
-SET gender=2  where person_id in (
-SELECT person_id
-FROM person
-GROUP BY person_id
-HAVING COUNT(name) > 1 OR COUNT(gender) > 1
+SET gender = (
+    SELECT MAX(gender)
+    FROM (
+        SELECT person_id, gender
+        FROM movie_crew 
+        UNION
+        SELECT person_id, gender
+        FROM movie_cast
+    ) AS combined_data
+    WHERE combined_data.person_id = movie_cast.person_id
+)
+WHERE person_id IN (
+    SELECT person_id
+    FROM Person
+    GROUP BY person_id
+    HAVING COUNT(DISTINCT gender) > 1
+);
+
+/*=========Αντικατάσταση των διπλότυπων με το πρώτο όνομα===========*/
+
+UPDATE movie_cast
+SET name = (
+    SELECT TOP(1) name
+    FROM (
+        SELECT person_id, name
+        FROM movie_crew 
+        UNION 
+        SELECT person_id, name
+        FROM movie_cast
+    ) AS combined_data
+    WHERE combined_data.person_id = movie_cast.person_id
+)
+WHERE person_id IN (
+    SELECT person_id
+    FROM Person
+    GROUP BY person_id
+    HAVING COUNT(DISTINCT name) > 1
 );
 
 UPDATE movie_crew
-SET gender=2  where person_id in (
-SELECT person_id
-FROM person
-GROUP BY person_id
-HAVING COUNT(name) > 1 OR COUNT(gender) > 1
+SET name = (
+    SELECT TOP(1) name
+    FROM (
+        SELECT person_id, name
+        FROM movie_crew 
+        UNION
+        SELECT person_id, name
+        FROM movie_cast
+    ) AS combined_data
+    WHERE combined_data.person_id = movie_crew.person_id
+)
+WHERE person_id IN (
+    SELECT person_id
+    FROM Person
+    GROUP BY person_id
+    HAVING COUNT(DISTINCT name) > 1
 );
 
-UPDATE movie_crew
-SET name='Ka-Fai Cheung'  where person_id in (
-SELECT person_id
-FROM person
-GROUP BY person_id
-HAVING COUNT(name) > 1 OR COUNT(gender) > 1
-);
+
+
